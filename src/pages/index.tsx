57d047e68c08627fc86989fe256eb68a4d81a510
s1F102301167 <136313317+s1F102301167@users.noreply.github.com>
2024-04-23 04:05:26 +0900

    途中経過(動いていない)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,23 +14,36 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const directions = [
-    [0,1],
-    [-1,1],
-    [-1,0],
-    [-1,-1],
-    [0,-1],
-    [1,-1],
-    [1,0],
-    [1,1],
-  ]
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    for (let )
-    if (board[y+1] !== undefined && board[y + 1][x] === 2 / turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
-      setBoard(newBoard);
+    for (const direction of directions) {
+      for (let i = 0; i < 8; i++) {
+        if (
+          board[y + direction[0]] !== undefined &&
+          board[y][x] === 0 &&
+          board[y + direction[0]][x + direction[1]] === 2 / turnColor &&
+          board[y + direction[0] * i][x + direction[1] * i] !== 0 &&
+          board[y + direction[0] * i][x + direction[1] * i] === turnColor
+        ) {
+          newBoard[y][x] = turnColor;
+          newBoard[y + direction[0]*i][x + direction[1]*i] = turnColor;
+          setTurnColor(2 / turnColor);
+          setBoard(newBoard);
+        } else {
+          setTurnColor(2 / turnColor);
+          setBoard(newBoard);
+        }
+      }
     }
   };
   return (
