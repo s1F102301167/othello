350414cbb360a9cddcc0a3ed79defc65a481935b
s1F102301167 <136313317+s1F102301167@users.noreply.github.com>
2024-04-24 14:46:36 +0900

    オセロ最初から

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,45 +24,15 @@ const Home = () => {
     [1, 1],
   ];
   const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
     const newBoard = structuredClone(board);
     if (newBoard[y][x] === 0) {
-      for (const direction of directions) {
-        for (let i = 1; i < 8; i++) {
-          if (newBoard[y + direction[0] * i] === undefined) {
-            break;
-          } else {
-            if (newBoard[y + direction[0] * i][x + direction[1] * i] === undefined) {
-              break;
-            } else if (newBoard[y + direction[0]][x + direction[1]] === turnColor) {
-              break;
-            } else if (newBoard[y + direction[0] * i][x + direction[1] * i] === 0) {
-              break;
-            } else if (
-              newBoard[y + direction[0] * i][x + direction[1] * i] === turnColor &&
-              i > 1
-            ) {
-              for (let j = i; j > 0; j--) {
-                newBoard[y + direction[0] * j][x + direction[1] * j] = turnColor;
-              }
-              newBoard[y][x] = turnColor;
-              setTurnColor(2 / turnColor);
-              setBoard(newBoard);
-            }
-          }
-        }
-      }
+      newBoard[y][x] === turnColor
+      setBoard(newBoard);
+      setTurnColor(3 - turnColor);
     }
   };
-  const point = (a: number) => board.flat().filter((b) => b === a).length;
   return (
     <div className={styles.container}>
-      <>
-        brack{point(1)}
-        /white{point(2)}
-        <br />
-        {['', 'brackturn', 'whiteturn'][turnColor]}
-      </>
       <div className={styles.boardStyle}>
         {board.map((row, y) =>
           row.map((color, x) => (
@@ -71,9 +41,7 @@ const Home = () => {
                 <div
                   className={styles.stoneStyle}
                   style={{
-                    background: color === 1 ? '#000' : color === 2 ? '#fff' : '#ff21ec',
-                    width: color === 3 ? '20px' : '56px',
-                    height: color === 3 ? '20px' : '56px',
+                    background: color === 1 ? '#000' : '#fff',
                   }}
                 />
               )}
