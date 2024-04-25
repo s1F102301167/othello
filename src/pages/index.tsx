764b78b260c943575d155c3c20d4d512861a642e
s1F102301167 <136313317+s1F102301167@users.noreply.github.com>
2024-04-25 22:50:32 +0900

    下におく＆エラー出ないところまでやった

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
     [0, 0, 0, 2, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 3],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const directions = [
     [0, 1],
@@ -26,9 +26,11 @@ const Home = () => {
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(board);
     if (newBoard[y][x] === 0) {
-      newBoard[y][x] === turnColor
-      setBoard(newBoard);
-      setTurnColor(3 - turnColor);
+      if (newBoard[y + 1] !== undefined && newBoard[y + 1][x] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
+      }
     }
   };
   return (
