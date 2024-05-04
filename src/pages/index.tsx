47d5d1d26b9cbb7a738c5918745ebb2c05c0b943
s1F102301167 <136313317+s1F102301167@users.noreply.github.com>
2024-05-05 01:25:46 +0900

    候補地途中経過

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home = () => {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
@@ -29,7 +29,7 @@ const Home = () => {
   ];
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(board);
-    if (newBoard[y][x] === 0) {
+    if (newBoard[y][x] === 0 || newBoard[y][x] === 3) {
       for (const direction of directions) {
         for (let i = 1; i < 8; i++) {
           if (
@@ -40,7 +40,10 @@ const Home = () => {
           ) {
             break;
           } else {
-            if (board[y + direction[0]][x + direction[1]] === 0) {
+            if (
+              board[y + direction[0]][x + direction[1]] === 0 ||
+              board[y + direction[0]][x + direction[1]] === 3
+            ) {
               break;
             } else if (board[y + direction[0]][x + direction[1]] === turnColor) {
               break;
@@ -55,11 +58,50 @@ const Home = () => {
             }
           }
         }
-        setBoard(newBoard);
-        setTurnColor(3 - turnColor);
       }
     }
+    for (let a = 0; a < 8; a++) {
+      for (let b = 0; b < 8; b++) {
+        if (board[a][b] === 3) {
+          newBoard[a][b] = 0;
+        }
+      }
+    }
+    newBoard[y][x] = turnColor;
+    setTurnColor(3 - turnColor);
+    for (let c = 0; c < 8; c++) {
+      for (let d = 0; d < 8; d++) {
+        if (newBoard[c][d] === 0) {
+          for (const direction of directions) {
+            for (let i = 1; i < 8; i++) {
+              if (
+                c + direction[0] * i < 0 ||
+                c + direction[0] * i >= 8 ||
+                d + direction[1] * i < 0 ||
+                d + direction[1] * i >= 8
+              ) {
+                break;
+              } else {
+                if (board[c + direction[0]][d + direction[1]] === 0) {
+                  break;
+                } else if (board[c + direction[0]][d + direction[1]] === turnColor) {
+                  break;
+                } else if (board[c + direction[0]][d + direction[1]] === 3 - turnColor) {
+                  if (board[c + direction[0] * i][d + direction[1] * i] === 0) {
+                    break;
+                  } else if (board[c + direction[0] * i][d + direction[1] * i] === turnColor) {
+                    newBoard[c][d] = 3;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.scoreboard}>
@@ -67,6 +109,7 @@ const Home = () => {
         <div className={styles.score}>
           黒の石: {blackCount} | 白の石: {whiteCount}
         </div>
+        <div>ピンク：おけるところ</div>
       </div>
       <div className={styles.boardStyle}>
         {board.map((row, y) =>
@@ -76,7 +119,7 @@ const Home = () => {
                 <div
                   className={styles.stoneStyle}
                   style={{
-                    background: color === 1 ? '#000' : '#fff',
+                    background: color === 1 ? '#000' : color === 2 ? '#fff' : '#FFC0CB',
                   }}
                 />
               )}
